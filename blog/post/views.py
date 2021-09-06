243f261245d07391449c90f96b4a3f1a7c8e4821
ArsenAjiev <selftcoder@gmail.com>
2021-09-06 08:35:25 +0300

    add html

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
-
 
 from post.models import Post
 
 
 def index(request):
-    title = request.GET.get("title")
-    post_list = Post.objects.filter(title__contains=title)
-    post_ids = [post.id for post in post_list]
-    return HttpResponse(", ".join(post_ids))
+    slug = request.GET.get("slug")
+
+    if request.user.is_authenticated:
+          post_list = Post.objects.filter(author=request.user)
+    else:
+        post_list = Post.objects.all()
+
+    if slug is not None:
+        post_list = post_list.filter(slug__contains=slug)
+
+    post_titles = [post.slug for post in post_list]
+    return HttpResponse(", ".join(post_titles))
 
 
 
